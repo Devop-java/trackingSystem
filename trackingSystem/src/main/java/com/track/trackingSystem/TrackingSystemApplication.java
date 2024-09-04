67a942dbd4292c08ad2800e267f3ff1612c3dab1
NIC_SUNIL <NIC_SUNIL@DESKTOP-VBOGB6D>
2024-09-04 15:28:49 +0530

    Commit Project

---
+++ trackingSystem/src/main/java/com/track/trackingSystem/TrackingSystemApplication.java
@@ -0,0 +1,26 @@
+package com.track.trackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class TrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingSystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "HEAD", "DELETE").allowedOrigins("*",
+						"http://localhost:8080/");
+			}
+		};
+	}
+}
